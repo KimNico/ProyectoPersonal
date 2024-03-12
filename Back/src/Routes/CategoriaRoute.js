41e97e2c5c53e9af6a09fccd1887c908b568b846
Nicolas Kim <kimnicolasalejandro@gmail.com>
2024-03-12 17:13:02 -0300

    add routes

---
+++ Back/src/Routes/CategoriaRoute.js
@@ -0,0 +1,11 @@
+const {Router} = require("express")
+
+const categoriaRoute  = Router()
+
+categoriaRoute.post('/')
+categoriaRoute.put('/')
+categoriaRoute.get('/')
+categoriaRoute.delete('/')
+
+module.exports = categoriaRoute;
+
