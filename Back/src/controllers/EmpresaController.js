a143264f56b3c46af2d5945ab09993c5427c50dd
Nicolas Kim <kimnicolasalejandro@gmail.com>
2024-04-08 15:07:15 -0300

    Empresa controller created

--- Back/src/controllers/EmpresaController.js
+++ Back/src/controllers/EmpresaController.js
@@ -1,9 +1,36 @@
+const { Empresa } =require('../db')
 const getEmpresaController = async (req,res) => {
     try {
-        res.status(200).send(`Empresa`);
+        const empresa = await Empresa.findAll()
+        res.json(empresa)
     }
     catch(error) {
-      res.status(404).json({ error: error.message });
+      res.status(500).json({ error: 'Error al cargar las empresas'});
+      console.log(error);
+    }
+  }
+
+  const getEmpresaByIdController = async (req,res)=>{
+    const {id} =req.params;
+    try {
+      const empresa = await findByPk(id)
+      if(!empresa.length){
+        res.status(404).json({error:'Empresa no encotrado'})
+      }else{
+        res.json(empresa)
+      }
+      
+    } catch (error) {
+      res.status(500).json({ error: 'Error al cargar las empresas'});
+      console.log(error);
+    }
+  }
+
+  const postEmpresaController = async (req,res)=>{
+    try {
+      
+    } catch (error) {
+      
     }
   }
   module.exports = {getEmpresaController}
