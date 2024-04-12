95d5f59aabbc1a3a22655d86ebc41210e94f4384
Nicolas Kim <kimnicolasalejandro@gmail.com>
2024-04-12 16:57:08 -0300

    actions redux actualizado

--- Front/src/redux/action.js
+++ Front/src/redux/action.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const GET_EMPRESA = GET_EMPRESA
-export const GET_EMPLEOS = GET_EMPLEOS
+export const GET_PUBLICACIONES = GET_PUBLICACIONES
 export const GET_USERS = GET_USERS
 export const GET_EMPLEOS_BY_ID = GET_EMPLEOS_BY_ID
 export const GET_USERS_BY_ID = GET_USERS_BY_ID
@@ -13,9 +13,8 @@ export const DELETE_EMPRESA = DELETE_EMPRESA
 export const getUsers = () => {
     return async function (dispatch) {
       const usersData = await axios.get(`/users`);
-      const users = usersData.data;
-      // console.log("get users result: ", users.length);
-      dispatch({ type: GET_USERS, payload: users });
+      const user = usersData.data;
+      dispatch({ type: GET_USERS, payload: user });
     };
   };
 
@@ -23,8 +22,44 @@ export const getUsers = () => {
     return async function (dispatch) {
       const userData = await axios.get(`/users/${id}`);
       const user = userData.data;
-      // console.log("get users result: ", user.length);
-      dispatch({ type: GET_USER, payload: user });
+      dispatch({ type: GET_USERS_BY_ID, payload: user });
+    };
+  };
+
+  export const getEmpresas = () => {
+    return async function (dispatch) {
+      const empresasData = await axios.get(`/empresa`);
+      const empresas = empresasData.data;
+      dispatch({ type: GET_EMPRESA, payload: empresas });
+    };
+  };
+
+  export const getEmpresaById = (id) => {
+    return async function (dispatch) {
+      const empresaData = await axios.get(`/empresa/${id}`);
+      const empresa = empresaData.data;
+      dispatch({ type: GET_EMPRESA_BY_ID, payload: empresa });
     };
   };
   
+  export const getPublicaciones = ()=>{
+    return async function(dispatch){
+      const publicacionData = await axios.get('/publicaciones')
+      const publicaciones = publicacionData.data
+      dispatch({type: GET_PUBLICACIONES,payload:publicaciones})
+    }
+  }
+
+  export const getPublicacionesById = (id)=>{
+    return async function(dispatch){
+      const publicacionData = await axios.get(`/publicaciones/${id}`)
+      const publicaciones = publicacionData.data
+      dispatch({type: GET_PUBLICACIONES,payload:publicaciones})
+    }
+  }
+  
+  export const deleteEmpresa = (id)=>{
+    return async function(dispatch){
+      await axios.delete()
+    }
+  }
