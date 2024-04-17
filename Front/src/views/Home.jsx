593c7514a9db8829cd1baf576b668d83e3abe99c
Nicolas Kim <kimnicolasalejandro@gmail.com>
2024-04-17 00:53:45 -0300

    installed MUI and navbar creation

--- Front/src/views/Home.jsx
+++ Front/src/views/Home.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export const Home = ()=>{
     return(
         <body>
-            <div>
-                Home
+            <div >
+                <Box
+                 component="form"
+                 sx={{
+                   '& .MuiTextField-root': { m: 1, width: '25ch' },
+                 }}
+                 noValidate
+                 autoComplete="off">
+            <TextField
+          label="Search"
+          id="standard-size-small"
+          defaultValue=""
+          size="small"
+          variant="standard"
+        />
+            </Box>
+                <SearchRoundedIcon/>
             </div>
         </body>
     )
