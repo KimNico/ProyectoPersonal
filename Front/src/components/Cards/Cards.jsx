039f24b44880244c81a3705fc74c4fbe08b56c23
Nicolas Kim <kimnicolasalejandro@gmail.com>
2024-08-12 23:02:12 -0300

    added fav button

--- Front/src/components/Cards/Cards.jsx
+++ Front/src/components/Cards/Cards.jsx
@@ -4,14 +4,20 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import { Link } from 'react-router-dom';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, IconButton } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite'; // Icono para el botón
 import styles from './Card.module.css'; 
 
-export const Cards = ({ cardsData }) => {
+export const Cards = ({ cardsData, onSave }) => {
+  // Verifica que cardsData no sea null o undefined
+  if (!cardsData || cardsData.length === 0) {
+    return <Typography variant="body1">No hay publicaciones disponibles</Typography>;
+  }
+
   return (
     <Grid container spacing={3}>
       {cardsData.map((publicacion) => (
-        <Grid item xs={12} sm={6} md={4} key={publicacion.id_publicacion}>
+        <Grid item xs={12} sm={6} md={4} key={publicacion.id_trabajo}>
           <CardActionArea component={Link} to={`/publicacion/${publicacion.id_trabajo}`}>
             <Card className={styles['card-container']}>
               <CardContent>
@@ -27,6 +33,19 @@ export const Cards = ({ cardsData }) => {
                 <Typography variant="body2" className={styles['card-text']}>
                   <strong>Modalidad:</strong> {publicacion.modalidad}
                 </Typography>
+                <IconButton 
+                  className={styles['save-button']}
+                  onClick={(e) => {
+                    e.stopPropagation(); // Evita que el clic en el botón navegue a la página
+                    if (onSave) {
+                      onSave(publicacion); // Asegúrate de que onSave esté definido
+                    } else {
+                      console.error('onSave function is not defined');
+                    }
+                  }}
+                >
+                  <FavoriteIcon />
+                </IconButton>
               </CardContent>
             </Card>
           </CardActionArea>
